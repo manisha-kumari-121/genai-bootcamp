f194c7c51a70110009e466fe10324ea76b3895c0
manisha-kumari-121 <manisha.kumari@tookitaki.com>
2025-08-16 15:41:55 +0530

    Added binary search code

--- binary_search.js
+++ binary_search.js
@@ -1,10 +1,15 @@
-function binarySearch(arr, x) {
-  let l = 0, r = arr.length - 1;
-  while (l <= r) {
-    let m = Math.floor((l + r) / 2);
-    if (arr[m] == x) return m;
-    if (arr[m] < x) l = m + 1;
-    else r = m - 1;
-  }
-  return -1;
+function binarySearch(arr, target){
+    let left = 0;
+    let right = arr.length - 1;
+    while(left <= right){
+        let mid = Math.floor((left + right) / 2);
+        if(arr[mid] === target){
+            return mid;
+        } else if(arr[mid] < target){
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+    return -1;
 }
